8be9f0961e1981595ae81fccb7cceec7c5906870
SebasAuM <aasebastiann@gmail.com>
2026-01-25 13:04:55 -0500

    feature : funcion combinar resultados hecha por carril 2- minimo y cantidad

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -25,7 +25,20 @@ public class CarreraParalela {
         
         System.out.println("Número más pequeño (Carril 2): " + min);
         System.out.println("Cantidad de datos ingresados: " + cantidad);
+    }
 
+    public static Resultado combinarResultados(List<Integer> lista) {
+    Resultado res = new Resultado();
+    res.minimo = lista.stream().min((a, b) -> a.compareTo(b)).orElse(0);
+    res.cantidad = lista.stream().count();
+
+    return res;
+    }
+
+    static class Resultado {
+        int maximo;
+        int minimo;
+        long cantidad;
     }
 
 }
