8d88ea1647c3b31cef3b1912aade684fd4c67131
Juan Angel Salas <juan.salas-g@mail.escuelaing.edu.co>
2026-01-25 12:26:07 -0500

    rama reto 2 -> rama carril1 -->merge de rama reto 2 sebastian  sobre carril1 --> funcion lambda -->commit

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,7 +1,24 @@
+import java.util.Arrays;
+
+import java.util.List;
 
 public class CarreraParalela {
+
     public static void main(String[] args) {
 
         System.out.println("Carrera en Paralelo ");
+
+        List<Integer> numeros = Arrays.asList(5, 12, 3, 89, 45, 23);
+
+        Integer max = numeros.stream()
+
+                .max((a, b) -> a.compareTo(b))
+
+                .orElse(0);
+
+        System.out.println("Numero mas grande (Carril 1): " + max);
+
     }
+
 }
+ 
