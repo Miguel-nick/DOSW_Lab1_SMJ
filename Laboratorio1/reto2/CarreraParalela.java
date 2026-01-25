6aaec4ecc819802d7a46cc6b236c75378dcf1670
SebasAuM <aasebastiann@gmail.com>
2026-01-25 16:39:21 -0500

    feature: La gran meta - funcionalidad completa con todas las verificaciones

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -7,7 +7,9 @@ public class CarreraParalela {
         int minimo;
         long cantidad;
         boolean maximoEsMultiploDe2;
+        boolean maximoEsDivisorDe2;  
         boolean cantidadEsPar;
+        boolean cantidadEsImpar;     
 
         @Override
         public String toString() {
@@ -15,6 +17,10 @@ public class CarreraParalela {
                     "maximo=" + maximo +
                     ", minimo=" + minimo +
                     ", cantidad=" + cantidad +
+                    ", maximoEsMultiploDe2=" + maximoEsMultiploDe2 +
+                    ", maximoEsDivisorDe2=" + maximoEsDivisorDe2 +
+                    ", cantidadEsPar=" + cantidadEsPar +
+                    ", cantidadEsImpar=" + cantidadEsImpar +
                     '}';
         }
     }
@@ -26,11 +32,10 @@ public class CarreraParalela {
             res.maximo = 0;
             res.minimo = 0;
             res.cantidad = 0;
-
-            // ternario (cumplir enunciado)
-            res.maximoEsMultiploDe2 = (res.maximo % 2 == 0) ? true : false;
-            res.cantidadEsPar = (res.cantidad % 2 == 0) ? true : false;
-
+            res.maximoEsMultiploDe2 = false;
+            res.maximoEsDivisorDe2 = false;
+            res.cantidadEsPar = true;  // 0 es par
+            res.cantidadEsImpar = false;
             return res;
         }
 
@@ -38,19 +43,24 @@ public class CarreraParalela {
         res.minimo = lista.stream().min(Integer::compareTo).orElse(0);
         res.cantidad = lista.size();
 
-        //  Segunda vuelta
+
         res.maximoEsMultiploDe2 = (res.maximo % 2 == 0) ? true : false;
 
-        //  Tercer choque
+        res.maximoEsDivisorDe2 = (res.maximo != 0) ? (2 % res.maximo == 0) : false;
+
         res.cantidadEsPar = (res.cantidad % 2 == 0) ? true : false;
 
+        res.cantidadEsImpar = (res.cantidad % 2 != 0) ? true : false;
+
         return res;
-
-
     }
 
     public static void main(String[] args) {
-        List<Integer> arr = List.of(45, 33, 345, 78, 7);
-        System.out.println(combinarResultados(arr));
+        // Probar con DOS listas
+        List<Integer> lista1 = List.of(45, 33, 345, 78, 7);
+        List<Integer> lista2 = List.of(10, 20, 30, 40);
+
+        System.out.println("Lista 1: " + combinarResultados(lista1));
+        System.out.println("Lista 2: " + combinarResultados(lista2));
     }
 }
