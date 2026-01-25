a0d82eef86036a7f1ab47a7898b3ec6da9601b1c
Juan Angel Salas <juan.salas-g@mail.escuelaing.edu.co>
2026-01-25 15:47:52 -0500

    Segunda vuelta y tercer choque carril 1

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -2,11 +2,12 @@ import java.util.List;
 
 public class CarreraParalela {
 
-
     public static class Resultado {
         int maximo;
         int minimo;
         long cantidad;
+        boolean maximoEsMultiploDe2;
+        boolean cantidadEsPar;
 
         @Override
         public String toString() {
@@ -21,34 +22,35 @@ public class CarreraParalela {
     public static Resultado combinarResultados(List<Integer> lista) {
         Resultado res = new Resultado();
 
-
         if (lista == null || lista.isEmpty()) {
             res.maximo = 0;
             res.minimo = 0;
             res.cantidad = 0;
+
+            // ternario (cumplir enunciado)
+            res.maximoEsMultiploDe2 = (res.maximo % 2 == 0) ? true : false;
+            res.cantidadEsPar = (res.cantidad % 2 == 0) ? true : false;
+
             return res;
         }
 
-
-        res.maximo = lista.stream()
-                .max(Integer::compareTo)
-                .orElse(0);
-
-
-        res.minimo = lista.stream()
-                .min(Integer::compareTo)
-                .orElse(0);
-
-
+        res.maximo = lista.stream().max(Integer::compareTo).orElse(0);
+        res.minimo = lista.stream().min(Integer::compareTo).orElse(0);
         res.cantidad = lista.size();
 
+        //  Segunda vuelta
+        res.maximoEsMultiploDe2 = (res.maximo % 2 == 0) ? true : false;
+
+        //  Tercer choque
+        res.cantidadEsPar = (res.cantidad % 2 == 0) ? true : false;
+
         return res;
+
+
     }
 
     public static void main(String[] args) {
         List<Integer> arr = List.of(45, 33, 345, 78, 7);
-
-        Resultado r = combinarResultados(arr);
-        System.out.println(r);
+        System.out.println(combinarResultados(arr));
     }
 }
