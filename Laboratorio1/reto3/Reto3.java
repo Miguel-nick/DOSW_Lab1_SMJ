4b6fc18d99b31acb6c43411dc6e3778b7847f716
unknown <juan.salas-g@labinfo.is.escuelaing.edu.co>
2026-01-27 15:34:11 -0500

    reto 3 algoritmo para utilizar StringBuffer

--- Laboratorio1/reto3/Reto3.java
+++ Laboratorio1/reto3/Reto3.java
@@ -1,1 +1,18 @@
- 
+import java.util.stream.IntStream;
+import java.util.function.Function;
+
+public class Reto3 {
+    public static String transformar(String mensaje) {
+        StringBuffer menB = new StringBuffer();
+        IntStream.range(0, 3)
+                .forEach(inti -> {
+                    if (i > 0) menB.append(" ");
+                    menB.append(mensaje);
+                });
+        return menB.toString();
+    }
+    public static void main(String[] args) {
+        Function<String, String> f = (String msg -> Reto3.transformar(msg);
+        System.out.println(f.apply("Hola"));
+    }
+}
