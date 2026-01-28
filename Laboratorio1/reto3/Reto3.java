d975bf3fc431e3255384237cd55b9b037f98b791
SebasAuM <Aasebastiann@gmail.com>
2026-01-27 21:51:53 -0500

    Reto3.java

--- Laboratorio1/reto3/Reto3.java
+++ Laboratorio1/reto3/Reto3.java
@@ -1,27 +1,19 @@
-import java.util.stream.IntStream;
 import java.util.function.Function;
-//Clase que muestra la estructura del reto3 pero con el uso de StringBuffer
-//@autor Miguel Angel Sandoval
 
 public class Reto3 {
-
-    //Trasforma un mensaje repitiendolo tres veces separado por espacios
-    //@param mensaje
-    //@return una cadena del mensaje 3 veces
+    /**
+     * Método transformar (Estudiante B)
+     * Usa StringBuffer para invertir el mensaje recibido.
+     *
+     * @param mensaje Texto de entrada
+     * @return Texto invertido
+     */
     public static String transformar(String mensaje) {
-        StringBuffer menB = new StringBuffer();
-        IntStream.range(0, 3)
-                .forEach(inti -> {
-                    if (i > 0) menB.append(" ");
-                    menB.append(mensaje);
-                });
-        return menB.toString();
+        StringBuffer buffer = new StringBuffer(mensaje);
+        return buffer.reverse().toString();
     }
-
-    //Metodo principal que imprime la palabra
     public static void main(String[] args) {
-        //Define una funcion que aplica la trasformacio 
-        Function<String, String> f = (String msg -> Reto3.transformar(msg);
+        Function<String, String> f = msg -> Reto3.transformar(msg);
         System.out.println(f.apply("Hola"));
     }
 }
